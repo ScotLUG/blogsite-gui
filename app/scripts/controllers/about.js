ebfc183b12c59c1e73764a0074baf54b0dd1011b
Peter Kinnaird <peter@peterkinnaird.eu>
2014-09-13 20:15:27 +0100

    Initial file structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blognihonioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blognihonioApp
+ */
+angular.module('blognihonioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
